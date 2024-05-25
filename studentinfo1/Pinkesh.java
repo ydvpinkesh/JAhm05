211d4653c9207b9f61f4a475cf2b004992d7c91f
ydvpinkesh <144173927+ydvpinkesh@users.noreply.github.com>
2024-05-25 21:26:13 +0530

    Add files via upload

---
+++ studentinfo1/Pinkesh.java
@@ -0,0 +1,17 @@
+package studentinfo1;
+
+public class Pinkesh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		student obj = new student();
+        obj.ss = "Dhaval";
+        student ss1 = new student();
+        System.out.println(obj.ss);
+        System.out.println(obj.name);
+
+  
+	}
+
+}
---
+++ studentinfo1/student.java
@@ -0,0 +1,14 @@
+package studentinfo1;
+
+public class student {
+
+	String ss;
+    String name;
+  public student(String ss){
+      name = ss;
+  }
+  public student()
+  {
+      name = "unknown";
+  }
+}
